aa52a2da6524acc944c6f6ec16556fb5046d3972
mayan <mshoshan@uci.edu>
2021-11-04 00:24:31 -0700

    update about page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>this is the web front end for the VR emergency training proof of concept project, if you've already created an account and done the training in VR app, you can login to see your data on the Login tab</h1>
+
+    <a href="https://github.com/shoshanimayan/VR-Emergency-Training">link the main projects github</a>
   </div>
 </template>
